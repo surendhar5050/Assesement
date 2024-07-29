ef6853159506a8f9bf9155b07f011c7227655ada
Surendhar <surendhar.B@fcsonline.co.in>
2024-07-29 11:27:53 +0530

    change

--- weather_based_news/android/app/src/main/kotlin/com/example/weather_based_news/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.weather_based_news
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- weather_based_news/lib/utils/colors.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppColor{
-
-  static final Color primaryColor=Color(0xFFFF8F00);
-}
